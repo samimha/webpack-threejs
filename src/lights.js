463197608e48e8d2b7576f387b6eb68322de38ec
Sami Haapanen <sami.haapanen@hotmail.com>
2019-04-15 11:49:16 +0300

    Edit lights

--- src/lights.js
+++ src/lights.js
@@ -1,13 +1,13 @@
 import * as THREE from "three";
 
 export const getPointLight = () => {
-  const pointLight = new THREE.PointLight("magenta", 9, 200);
+  const pointLight = new THREE.PointLight("white", 2, 200);
   pointLight.position.set(10, 10, 35);
   return pointLight;
 };
 
 export const getPointLightCyan = () => {
-  const pointLight = new THREE.PointLight("cyan", 9, 200);
+  const pointLight = new THREE.PointLight("cyan", 2, 200);
   pointLight.position.set(-10, -10, -35);
   return pointLight;
 };
