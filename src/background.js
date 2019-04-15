4d55e5141a6dc170e41ee956434f033563e308f0
Sami Haapanen <sami.haapanen@hotmail.com>
2019-04-15 10:46:38 +0300

    Edit background scale and light distance

--- src/background.js
+++ src/background.js
@@ -3,7 +3,7 @@ import galaxy_starfield from "./textures/galaxy_starfield.png";
 
 export const getBackground = () => {
   const galaxyBackground = new THREE.TextureLoader().load(galaxy_starfield);
-  const galaxyGeometry = new THREE.SphereGeometry(60, 64, 64);
+  const galaxyGeometry = new THREE.SphereGeometry(150, 64, 64);
   const galaxyMaterial = new THREE.MeshPhongMaterial({
     transparent: true,
     map: galaxyBackground,
